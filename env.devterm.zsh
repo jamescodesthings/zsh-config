1fea111183d08970c078864a89746debe224c43d
JamesCodesThings <jamescodesthings@gmail.com>
2022-07-27 14:14:04 +0000

    update devterm env config

--- env.devterm.zsh
+++ env.devterm.zsh
@@ -5,3 +5,7 @@ alias battery-raw="cat /sys/class/power_supply/axp20x-battery/capacity"
 alias battery="echo \"$(battery-raw)%\""
 
 alias visudo="sudo EDITOR=nano /usr/bin/visudo"
+
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+export LDFLAGS=-fuse-ld=lld
