a1cfdb547c8c251b108d642d7de05ddb82bc2ebe
james <James Macmillan>
2023-08-17 09:33:55 +0100

    add navi integration

--- env.james-xps.zsh
+++ env.james-xps.zsh
@@ -2,8 +2,11 @@
 
 export PATH="$HOME/.bin:$PATH"
 export PATH="$PATH:/opt/mssql-tools/bin"
+export PATH="${PATH}:/home/james/.cargo/bin"
 
 alias ls='exa --group-directories-first'
 
 export HAS_RUN_ENV=true
+
+alias BarbieGirl='echo QgBhAHIAYgBpAGUARwBpAHIAbAA='
 
