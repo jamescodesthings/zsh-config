76c93eff28586cd0e139019875a1c4f3ed5fd4c9
jamescodesthings <jamescodesthings@gmail.com>
2024-10-27 13:42:54 +0000

    Implement trimmed project name and add new LÖVE template files

---
+++ templates/love/config.lua
@@ -0,0 +1,7 @@
+function love.conf(w)
+  w.window.title = "$PROJECT_NAME_PRETTY"
+  w.window.width = 800
+  w.window.height = 600
+  w.window.resizable = false
+  w.window.fullscreen = false
+end
---
+++ templates/love/main.lua
@@ -0,0 +1,16 @@
+---@diagnostic disable: duplicate-set-field
+package.path = package.path .. ";./lib/?.lua"
+
+--$PROJECT_NAME_PRETTY
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
