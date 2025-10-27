5a73cac7f687b56a5c19b17487a06c994af4ed07
James Macmillan <jamescodesthings@gmail.com>
2025-10-27 12:04:43 +0000

    fix asdf-direnv

--- load-direnv.zsh
+++ load-direnv.zsh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
-if is existing direnv; then
-  eval "$(direnv hook zsh)"
+ASDF_DIRENV=$(asdf which direnv 2> /dev/null)
+
+if [ -n "$ASDF_DIRENV" ]; then
+  eval "$($ASDF_DIRENV hook zsh)"
+else
+  echo "direnv not found"
 fi
