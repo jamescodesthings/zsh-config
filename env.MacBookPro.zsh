2218495f8b0a7cb5781763cbe27656655c859be7
James Macmillan <jamescodesthings@gmail.com>
2026-04-27 17:42:17 +0100

    pico8 helpers

--- env.MacBookPro.zsh
+++ env.MacBookPro.zsh
@@ -88,8 +88,12 @@ export LC_CTYPE=en_GB.UTF-8
 export BUTANO_DIR="/Users/jamesmacmillan/projects/third-party/butano/butano"
 export BUTANO_TEMPLATE_DIR="/Users/jamesmacmillan/projects/third-party/butano/template"
 
-alias pico8="/Applications/PICO-8.app/Contents/MacOS/pico8"
 export PICO8="/Applications/PICO-8.app/Contents/MacOS/pico8"
+alias pico="nohup $PICO8 &"
+
+function picoh() {
+  nohup $PICO8 -root_path "$(pwd)" &
+}
 
 export GPT_HAS_NO_INTERACTION=1
 
