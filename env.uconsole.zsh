6b9639a43fa5c73e3f2a48c7928b321cc9cd1b41
James Macmillan <jamescodesthings@gmail.com>
2026-04-28 14:53:22 +0100

    add pico8 to uconsole

--- env.uconsole.zsh
+++ env.uconsole.zsh
@@ -4,10 +4,9 @@ alias wifi="nmcli dev wifi list"
 alias battery="upower -d"
 alias bat=battery
 
-export PICO8="/home/james/apps/pico-8/pico8_64"
-
 export DOCKER_HOST=unix:///run/user/1000/docker.sock
 export PATH="$HOME/.local/bin:$PATH"
+export PATH="/home/james/projects/pico-8:$PATH"
 
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
