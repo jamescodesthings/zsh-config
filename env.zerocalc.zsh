4a3f5b5e5219244e50ffc5c737d1e0c01b050e27
James Macmillan <jamescodesthings@gmail.com>
2026-05-30 20:07:40 +0100

    move pico8

--- env.zerocalc.zsh
+++ env.zerocalc.zsh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 
 export PATH="$HOME/.local/bin:$PATH"
-export PATH="/home/james/projects/pico-8-bin:$PATH"
+export PATH="/home/james/projects/pico8bin:$PATH"
 
-export PICO8="/home/james/projects/pico-8-bin/pico8"
+export PICO8="/home/james/projects/pico8bin/pico8"
 
+alias hlp='ls -1 /usr/local/bin'
