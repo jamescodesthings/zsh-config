2f5690c3c62aadbaee1a2c634b7aad5e3f6df641
James Macmillan <jamescodesthings@gmail.com>
2026-06-05 12:54:09 +0100

    add pnpm to ubuntu

--- env.ubuntu.zsh
+++ env.ubuntu.zsh
@@ -4,3 +4,12 @@ export PATH="$HOME/.local/bin:$PATH"
 
 export CHEATSHEET_DIR="/home/james/projects/personal/cheatsheets"
 
+# eww
+# pnpm
+export PNPM_HOME="/home/james/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+case ":$PATH:" in
+  *":$PNPM_HOME/bin:"*) ;;
+  *) export PATH="$PNPM_HOME/bin:$PATH" ;;
+esac
+# pnpm end
