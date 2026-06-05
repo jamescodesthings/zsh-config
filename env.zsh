0dce72645e479a15f6ae1c3c677a69852f0077a5
James Macmillan <jamescodesthings@gmail.com>
2026-06-05 11:42:33 +0100

    ignore docs, update env and fix kitty conf

--- env.zsh
+++ env.zsh
@@ -21,6 +21,9 @@ export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
 
 export MICRO_TRUECOLOR=1
 
+# Disable pager for commands that use it by default, I like to use the terminal's scrollback instead
+export PAGER=
+
 # Set the editor to micro if it's available
 if is available micro; then
   export EDITOR="micro"
